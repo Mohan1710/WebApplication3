b52d611eacd9cad24afebaafe2a3bb020973cff8
Mohan1710 <73586054+Mohan1710@users.noreply.github.com>
2020-11-03 12:07:43 +0530

    add

--- WebForm1.aspx.cs
+++ WebForm1.aspx.cs
@@ -11,7 +11,10 @@ namespace WebApplication3
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if(!IsPostBack)
+            {
 
+            }
         }
     }
 }
